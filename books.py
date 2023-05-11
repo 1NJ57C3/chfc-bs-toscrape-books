fe3f55e763b362395db945556ab04171344deb94
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2023-05-11 10:45:51 -0700

    Corrected misunderstood parameters

--- books.py
+++ books.py
@@ -15,7 +15,7 @@ URL = "https://books.toscrape.com/"
 
 with open('books.csv', 'w') as output_file:
   to_csv = writer(output_file)
-  headers = ['Book', 'Image_URL', 'Rating', 'Price', 'In_Stock?', 'Category', 'Page']
+  headers = ['image_url', 'star_rating', 'price', 'in_stock?', 'category', 'page_url']
   to_csv.writerow(headers)
 
   response = request.urlopen(URL)
@@ -34,21 +34,20 @@ with open('books.csv', 'w') as output_file:
     category = document.find("h1").string
     paginator = document.find("ul", class_="pager")
 
-    page_number = paginator.find("li", class_="current").string.strip()[5:] if paginator else '1 of 1'
     next_page_element = paginator and paginator.find("a", string="next")
     next_page = next_page_element['href'] if next_page_element else ''
 
     for book in books:
       image_element = book.find("img")
 
-      name = image_element['alt']
       image_url = URL + image_element['src'].replace('../', '')
       rating = book.find("p", class_=re.compile("star-rating.*", flags=re.IGNORECASE))['class'][1].lower()
       rating_number = numerical_words[rating]
       price = book.find("p", class_=re.compile("price.*", flags=re.IGNORECASE)).string
       stock_status = book.find("p", class_=re.compile(".*availability", flags=re.IGNORECASE)).get_text().strip().lower() == "in stock"
+      page_url = URL + 'catalogue/' + book.find("h3").a['href'].replace('../', '')
 
-      to_csv.writerow([name, image_url, rating_number, price, stock_status, category, page_number])
+      to_csv.writerow([image_url, rating_number, price, stock_status, category, page_url])
 
     if next_page:
       return book_scraper(url, next_page)
